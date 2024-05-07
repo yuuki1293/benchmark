58e86c13c546310d524d86c5fa0a9735c768a830
yuuki1293 <yuuki1293@outlook.jp>
2024-05-07 21:58:59 +0900

    C89対応

--- convert_uint8_t_to_float32/main.h
+++ convert_uint8_t_to_float32/main.h
@@ -5,22 +5,22 @@
 #include <inttypes.h>
 #include <limits.h>
 
-void init(uint8_t *const src, size_t length);
+void init(uint8_t *const src, int length);
 
-void bench(void (*target)(const uint8_t *src, float *dst, size_t length), const char* const label, size_t count, int log);
+void bench(void (*target)(const uint8_t *src, float *dst, int length), const char* const label, int count, int log);
 
-void general(const uint8_t *src, float *dst, size_t length);
+void general(const uint8_t *src, float *dst, int length);
 
-void look_up_list(const uint8_t *src, float *dst, size_t length);
+void look_up_list(const uint8_t *src, float *dst, int length);
 
-void avx(const uint8_t *src, float *dst, size_t length);
+void avx(const uint8_t *src, float *dst, int length);
 
 #ifdef __AVX2__
-void avx2(const uint8_t *src, float *dst, size_t length);
+void avx2(const uint8_t *src, float *dst, int length);
 #endif
 
 #ifdef __AVX512F__
-void avx512(const uint8_t *src, float *dst, size_t length);
+void avx512(const uint8_t *src, float *dst, int length);
 #endif
 
-#endif//_MAIN_H_
+#endif
